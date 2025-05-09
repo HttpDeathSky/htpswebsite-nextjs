aacd6033e857fbf5c3530fa19220ada3b71f6a76
HtpsDeathSky <1752405558@qq.com>
2025-05-09 10:10:48 +0800

    update nextjs config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   basePath: '/website'
+   basePath: '/website',
+   images: {
+    domains: ['htpsdeathsky.top'],
+  },
 };
 
 export default nextConfig;
